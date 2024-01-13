6e0db41d8e164d4f540cec7ca4bb8c110cc006a5
jhon_macias@hotmail.es <jhonmacias@Jhons-MacBook-Pro.local>
2024-01-13 12:54:26 +0100

    actualizando componentes

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -1,20 +1,7 @@
-import Image from "next/image";
 export default function Navbar() {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image
-            src="https://flowbite.com/docs/images/logo.svg"
-            width={100}
-            height={100}
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Jhon Macias
-          </span>
-        </a>
+    <nav className="bg-grey border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap justify-items-center mx-auto p-4">
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -39,17 +26,8 @@ export default function Navbar() {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className="hidden w-full md:block md:w-auto " id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
             <li>
               <a
                 href="#"
