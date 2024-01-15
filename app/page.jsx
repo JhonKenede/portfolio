17f9e1c4a39a835daad20af3f5a4e063fc973f3c
jhon_macias@hotmail.es <jhonmacias@Jhons-MacBook-Pro.local>
2024-01-15 20:58:20 +0100

    modificando la estructura y los componentes

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  console.log("Ruta de la imagen:", "/prueba.jpeg");
+
+  return (
+    <section className="py-24">
+      <div className="container ">
+        <h1 className="text-3xl font-bold">Proyectos</h1>
+      </div>
+    </section>
+  );
+}
---
+++ app/slides/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { Autoplay, FreeMode } from "swiper/modules";
+import { images } from "@/lib/images";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export default function Page() {
+  return (
+    <section className="py-12">
+      <div>
+        <Swiper
+          grabCursor={true}
+          navigation={true}
+          pagination={{ clickable: true }}
+          modules={[FreeMode, Autoplay, Navigation, Pagination]}
+          slidesPerView={"auto"}
+          freeMode={true}
+          autoplay={{ delay: 5500 }}
+          className="h-96 w-full  md:w-[52%] xl:w-[30%] rounded-lg"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <div className="flex items-center justify-center w-[100%] md:w-[100%] mb-8  xl:w-[100%] ">
+                <Image src={image.src} alt={image.alt} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+}
